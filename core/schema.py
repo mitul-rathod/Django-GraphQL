79576966e3d7867211ff83bb0f53fb5a505d5421
Mitul Rathod <mitul.r@simformsolutions.com>
2023-05-27 15:45:21 +0530

    Added : JWT & CRUD Functionality

---
+++ core/schema.py
@@ -0,0 +1,17 @@
+import graphene
+
+import blog.schema
+import users.schema
+
+
+class Query(blog.schema.Query, users.schema.Query, graphene.ObjectType):
+    # Combine the queries from different apps
+    pass
+
+
+class Mutation(blog.schema.Mutation, users.schema.Mutation, graphene.ObjectType):
+    # Combine the mutations from different apps
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
